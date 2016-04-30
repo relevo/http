53bf3c2bd9e53f0a270ae875d395445c5b0908b9
Jan-Paul Kleemans <jpkleemans@gmail.com>
2016-04-30 16:55:43 +0200

    Add comment

--- src/RoutesHttpRequests.php
+++ src/RoutesHttpRequests.php
@@ -5,5 +5,10 @@ namespace Relevo\Http;
 
 interface RoutesHttpRequests
 {
+    /**
+     * Route http requests to middleware.
+     *
+     * @param HttpRequestRouter $router
+     */
     public function routeHttpRequests(HttpRequestRouter $router);
 }
