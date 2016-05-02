a8cbbbfb9f9949c84164b6e083f41b538dba0973
Jan-Paul Kleemans <jpkleemans@gmail.com>
2016-05-02 10:33:40 +0200

    Add put and delete to RequestRouter

--- src/RequestRouter.php
+++ src/RequestRouter.php
@@ -8,4 +8,8 @@ interface RequestRouter
     public function get(string $path, array $middleware, string $name = null);
 
     public function post(string $path, array $middleware, string $name = null);
+
+    public function put(string $path, array $middleware, string $name = null);
+
+    public function delete(string $path, array $middleware, string $name = null);
 }
