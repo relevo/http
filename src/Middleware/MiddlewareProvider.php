2804eaa075e5e8e3098aa382e07ebac721b08a65
Jan-Paul Kleemans <jpkleemans@gmail.com>
2016-05-02 13:49:39 +0200

    Rename interfaces

--- src/Middleware/MiddlewareProvider.php
+++ src/Middleware/MiddlewareProvider.php
@@ -3,7 +3,7 @@ declare(strict_types = 1);
 
 namespace Relevo\Http\Middleware;
 
-interface AddsMiddleware
+interface MiddlewareProvider
 {
     /**
      * Add middleware to pipeline.
--- src/Routing/RequestRoutesProvider.php
+++ src/Routing/RequestRoutesProvider.php
@@ -3,7 +3,7 @@ declare(strict_types = 1);
 
 namespace Relevo\Http\Routing;
 
-interface AddsRequestRoutes
+interface RequestRoutesProvider
 {
     /**
      * Add request routes to router.
