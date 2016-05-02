9741a5d17557a9dbffce1aa7ed371f09e3dd5e5a
Jan-Paul Kleemans <jpkleemans@gmail.com>
2016-05-02 11:27:59 +0200

    Add comments to RequestRouter

--- src/Routing/RequestRouter.php
+++ src/Routing/RequestRouter.php
@@ -5,13 +5,28 @@ namespace Relevo\Http\Routing;
 
 interface RequestRouter
 {
+    /**
+     * Route http requests to middleware.
+     */
     public function route(array $methods, string $path, array $middleware, string $name = null);
 
+    /**
+     * Route GET requests to middleware.
+     */
     public function get(string $path, array $middleware, string $name = null);
 
+    /**
+     * Route POST requests to middleware.
+     */
     public function post(string $path, array $middleware, string $name = null);
 
+    /**
+     * Route PUT requests to middleware.
+     */
     public function put(string $path, array $middleware, string $name = null);
 
+    /**
+     * Route DELETE requests to middleware.
+     */
     public function delete(string $path, array $middleware, string $name = null);
 }
