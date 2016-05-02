251615073c7054cdc6c2fab95bc822e4846dc212
Jan-Paul Kleemans <jpkleemans@gmail.com>
2016-05-02 11:31:36 +0200

    Add method params to RequestRouter comments

--- src/Routing/RequestRouter.php
+++ src/Routing/RequestRouter.php
@@ -7,26 +7,47 @@ interface RequestRouter
 {
     /**
      * Route http request to middleware.
+     *
+     * @param array $methods
+     * @param string $path
+     * @param array $middleware
+     * @param string $name
      */
     public function route(array $methods, string $path, array $middleware, string $name = null);
 
     /**
      * Route GET request to middleware.
+     *
+     * @param string $path
+     * @param array $middleware
+     * @param string $name
      */
     public function get(string $path, array $middleware, string $name = null);
 
     /**
      * Route POST request to middleware.
+     *
+     * @param string $path
+     * @param array $middleware
+     * @param string $name
      */
     public function post(string $path, array $middleware, string $name = null);
 
     /**
      * Route PUT request to middleware.
+     *
+     * @param string $path
+     * @param array $middleware
+     * @param string $name
      */
     public function put(string $path, array $middleware, string $name = null);
 
     /**
      * Route DELETE request to middleware.
+     *
+     * @param string $path
+     * @param array $middleware
+     * @param string $name
      */
     public function delete(string $path, array $middleware, string $name = null);
 }
