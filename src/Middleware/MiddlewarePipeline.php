9ddf3516ac2761f70f2d46cd71ae7e4c1ddeb956
Jan-Paul Kleemans <jpkleemans@gmail.com>
2016-05-10 14:58:45 +0200

    Refactor middleware pipeline interface to class

--- src/Middleware/MiddlewarePipeline.php
+++ src/Middleware/MiddlewarePipeline.php
@@ -3,19 +3,40 @@ declare(strict_types = 1);
 
 namespace Relevo\Http\Middleware;
 
-interface MiddlewarePipeline
+class MiddlewarePipeline
 {
+    /**
+     * @var string[]
+     */
+    private $middleware;
+
+    /**
+     * Create new MiddlewarePipeline.
+     *
+     * @param string[] $middleware
+     */
+    public function __construct(array $middleware = [])
+    {
+        $this->middleware = $middleware;
+    }
+
     /**
      * Add middleware.
      *
      * @param string $middleware
      */
-    public function add($middleware);
+    public function add(string $middleware)
+    {
+        $this->middleware[] = $middleware;
+    }
 
     /**
      * Get all middleware as an array.
      *
      * @return string[]
      */
-    public function toArray() : array;
+    public function toArray() : array
+    {
+        return $this->middleware;
+    }
 }
